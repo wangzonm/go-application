1df1cd3dbc93bea8c0481b220a7c1a3bbe52a7ff
Alexdener <476934077@qq.com>
2023-03-26 21:40:24 +0800

    concurrentmapV2

--- map/concurrentmap/concurrentmap_test.go
+++ map/concurrentmap/concurrentmap_test.go
@@ -6,6 +6,8 @@ import (
 	"strconv"
 	"sync"
 	"testing"
+
+	"github.com/orcaman/concurrent-map/v2"
 )
 
 var (
@@ -18,18 +20,23 @@ goos: darwin
 goarch: amd64
 pkg: map/concurrentmap
 cpu: Intel(R) Core(TM) i5-5287U CPU @ 2.90GHz
-BenchmarkConcurrentMap/0-4 	     100	  67790847 ns/op	 3999277 B/op	  499276 allocs/op
-BenchmarkConcurrentMap/1-4 	     100	  51639550 ns/op	 3998421 B/op	  499269 allocs/op
-BenchmarkConcurrentMap/2-4 	     100	  58651565 ns/op	 3998027 B/op	  499273 allocs/op
-BenchmarkConcurrentMap/3-4 	     100	  52596867 ns/op	 3997472 B/op	  499259 allocs/op
-BenchmarkConcurrentMap/4-4 	     100	  87013303 ns/op	 3998057 B/op	  499270 allocs/op
-BenchmarkSyncMap/0-4       	     100	  69154131 ns/op	 7194667 B/op	  699240 allocs/op
-BenchmarkSyncMap/1-4       	     100	  84881340 ns/op	 7195059 B/op	  699240 allocs/op
-BenchmarkSyncMap/2-4       	     100	  67451353 ns/op	 7195029 B/op	  699246 allocs/op
-BenchmarkSyncMap/3-4       	     100	  80630496 ns/op	 7194917 B/op	  699248 allocs/op
-BenchmarkSyncMap/4-4       	     100	 115386835 ns/op	 7195906 B/op	  699254 allocs/op
+BenchmarkConcurrentMap/0-4   	     100	  95876518 ns/op	 3999620 B/op	  499280 allocs/op
+BenchmarkConcurrentMap/1-4   	     100	  64931553 ns/op	 3999169 B/op	  499275 allocs/op
+BenchmarkConcurrentMap/2-4   	     100	  66856476 ns/op	 3998267 B/op	  499273 allocs/op
+BenchmarkConcurrentMap/3-4   	     100	 117061303 ns/op	 3997555 B/op	  499259 allocs/op
+BenchmarkConcurrentMap/4-4   	     100	  81363032 ns/op	 3998085 B/op	  499270 allocs/op
+BenchmarkSyncMap/0-4         	     100	 100979086 ns/op	 7195090 B/op	  699244 allocs/op
+BenchmarkSyncMap/1-4         	     100	  95963044 ns/op	 7195327 B/op	  699242 allocs/op
+BenchmarkSyncMap/2-4         	     100	  84743453 ns/op	 7195498 B/op	  699250 allocs/op
+BenchmarkSyncMap/3-4         	     100	  81156601 ns/op	 7194961 B/op	  699248 allocs/op
+BenchmarkSyncMap/4-4         	     100	  76454971 ns/op	 7194985 B/op	  699246 allocs/op
+BenchmarkConcurrentMapV2/0-4 	     100	  53158915 ns/op	 3200622 B/op	  399528 allocs/op
+BenchmarkConcurrentMapV2/1-4 	     100	  53396233 ns/op	 3200128 B/op	  399528 allocs/op
+BenchmarkConcurrentMapV2/2-4 	     100	  53511853 ns/op	 3200216 B/op	  399527 allocs/op
+BenchmarkConcurrentMapV2/3-4 	     100	  55735606 ns/op	 3200328 B/op	  399529 allocs/op
+BenchmarkConcurrentMapV2/4-4 	     100	  53184668 ns/op	 3200624 B/op	  399529 allocs/op
 PASS
-ok  	map/concurrentmap	74.638s
+ok  	map/concurrentmap	114.788s
 */
 func BenchmarkConcurrentMap(b *testing.B) {
 	cm := New()
@@ -83,6 +90,36 @@ func BenchmarkSyncMap(b *testing.B) {
 					defer wg.Done()
 					for i := 0; i < num; i++ {
 						sm.Load(fmt.Sprintf("k-%d", rand.Intn(num)))
+					}
+				}()
+			}
+			wg.Wait()
+		})
+	}
+}
+
+func BenchmarkConcurrentMapV2(b *testing.B) {
+	m := cmap.New[int]()
+	for i := 1; i <= num; i++ {
+		m.Set(fmt.Sprintf("k-%d", i), i)
+	}
+	b.ResetTimer()
+	for i := 0; i < 5; i++ {
+		b.Run(strconv.Itoa(i), func(b *testing.B) {
+			b.N = bN
+			wg := sync.WaitGroup{}
+			wg.Add(2 * b.N)
+			for i := 0; i < b.N; i++ {
+				go func() {
+					defer wg.Done()
+					for i := 0; i < num; i++ {
+						m.Set(fmt.Sprintf("k-%d", rand.Intn(num)), i)
+					}
+				}()
+				go func() {
+					defer wg.Done()
+					for i := 0; i < num; i++ {
+						_, _ = m.Get(fmt.Sprintf("k-%d", rand.Intn(num)))
 					}
 				}()
 			}
--- map/go.mod
+++ map/go.mod
@@ -1,3 +1,5 @@
 module map
 
 go 1.18
+
+require github.com/orcaman/concurrent-map/v2 v2.0.1 // indirect
